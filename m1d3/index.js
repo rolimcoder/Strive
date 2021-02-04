2531063f39a94554ab37757946731e4443cee834
Marcio Rolim <marcio.rolim@sky.com>
2021-02-04 16:54:56 +0100

    First file commit

---
+++ m1d3/index.js
@@ -0,0 +1,200 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be in JavaScript
+- You can ask for tutor's help
+- The solution must be available for the tutors by the end of the day
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+- You can test your code in a separate file or commenting the single parts in this file or directly in the Developer Console or in the Node Console.
+*/
+
+
+/* EXERCISE 1
+Create and array containing the first 5 positive numbers.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+//const positivenumbers = [1, 2, 3, 4, 5]
+//console.log('ex 1', positivenumbers)
+
+
+/* EXERCISE 2
+Create an object containing your name, surname, email address and age.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+//let mydetails = {
+//    name: "Marcio",
+//  surname: "Rolim",
+//   emailaddress: "marciorolim@sky.com",
+//   age: "26" 
+//}
+// console.log ("ex 2")
+// console.log (mydetails.name)
+//console.log ( mydetails.surname)
+//console.log (mydetails.emailaddress)
+//console.log (mydetails.age)
+
+/* EXERCISE 3
+Add to the previously created object a boolean value to rappresent wheter you have or not a driving license.
+*/
+
+/* WRITE YOUR CODE HERE */
+let mydetails = {
+    hasdrivinglicense: true,
+}
+     
+    
+/* EXERCISE 4
+Remove from the previously created object the age property.
+*/ 
+
+/* WRITE YOUR CODE HERE */
+// mydetails.pop()
+
+/* EXERCISE 5
+Create a second object with name, surname, email address and verify that this object has a different email address than the previous one.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+// let mydetaisl2 = {
+    name: "Marcio",
+    //  surname: "Rolim",
+    //   emailaddress: "marciorolim@sky.com",
+    //   age: "26
+}
+
+//  console.log 
+
+
+
+/* EXERCISE 6
+You are working for a eCommerce. In the variable totalShoppingCart you have the total amount spent by the current user.
+In your eCommerce you have a promotion: if the customer shopping cart is more than 50, they are eligible for free shipping (otherwise it costs 10).
+Write an algorithm that calculate totalCost based on this assumption.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 7
+You are working for the same eCommerce. Today is the black friday and everything has a 20% discount at the end of the purchase.
+Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 8
+Create an object rapresenting a car with properties like brand, model, licensePlate.
+After you create the first car, clone it 5 times and change the licensePlate for each cloned car without affecting the original one.
+*/
+
+/* WRITE YOUR CODE HERE */
+// let carproperties = {
+   carbrand:
+   carmodel:
+   Licenseplate:9876
+
+}
+
+// let carproperties2 = {
+    carbrand:
+    carmodel:
+    Licenseplate:5679
+}     
+
+// let carproperties3 = {
+    carbrand:
+    carmodel:
+    Licenseplate: 4567
+} 
+
+// let carproperties4 = {
+    carbrand:
+    carmodel:
+    Licenseplate: 2345
+} 
+
+// let carproperties5 = {
+    carbrand:
+    carmodel:
+    Licenseplate: 1234
+} 
+
+/* EXERCISE 9
+Create a new array called carsForRent containing all the cars from the previous exercise.
+*/ let carsForRent = {
+    // let carsForRent = {
+   carbrand:
+   carmodel:
+   Licenseplate:9876
+
+}
+
+// let carsForRent2 = {
+    carbrand:
+    carmodel:
+    Licenseplate:5679
+}     
+
+// let carproperties3 = {
+    carbrand:
+    carmodel:
+    Licenseplate: 4567
+} 
+
+// let carsForRent3 = {
+    carbrand:
+    carmodel:
+    Licenseplate: 2345
+} 
+
+// let carsForRent4 = {
+    carbrand:
+    carmodel:
+    Licenseplate: 1234
+} 
+
+// let carsForRent5 = {
+    carbrand:
+    carmodel:
+    Licenseplate: 1234
+
+}
+
+consolelog (mydetails.pop())
+
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 10
+Remove the first and the last car from the carsForRent array.
+*/ consolelog (carsForRent5.pop())
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 11
+Print in the console the TYPES of a car, of the licensePlate and of the brand properties.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 12
+Create a new array called carsForSale and insert 3 cars in it.
+Store in a variable totalCars the number of cars present in both carsForSale and carsForRent arrays.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 13
+Print in the console the data from each car in the carsForSale array.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* WHEN YOU ARE FINISHED
+Send the code via Discord to the tutor! In the next days we'll also learn how to use GIT
+*/
+
+
